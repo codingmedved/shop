eb353a04617e6dff43447aeebed627d5dbea38c7
django <django>
2017-02-05 13:36:20 +0200

    Added improvements to models in save method and post-save signal

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.db.models.signals import post_save
 
 
 class Status(models.Model):
@@ -34,6 +35,10 @@ class Order(models.Model):
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+    def save(self, *args, **kwargs):
+
+        super(Order, self).save(*args, **kwargs)
+
 
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, blank=True, null=True, default=None)
@@ -49,5 +54,29 @@ class ProductInOrder(models.Model):
         return "%s" % self.product.name
 
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+
+    def save(self, *args, **kwargs):
+        price_per_item = self.product.price
+        self.price_per_item = price_per_item
+        self.total_price = self.nmb * price_per_item
+
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+    order = instance.order
+    all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+    order_total_price = 0
+    for item in all_products_in_order:
+        order_total_price += item.total_price
+
+    instance.order.total_price = order_total_price
+    instance.order.save(force_update=True)
+
+
+post_save.connect(product_in_order_post_save, sender=ProductInOrder)
--- products/models.py
+++ products/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "%s" % self.name
+        return "%s, %s" % (self.price, self.name)
 
     class Meta:
         verbose_name = 'Товар'
