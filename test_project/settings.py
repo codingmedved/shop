7db769b01404e9932ae88dc9d24fcaeff1b90a40
django <django>
2017-05-31 23:48:05 +0300

    Update for server

--- test_project/settings.py
+++ test_project/settings.py
@@ -137,3 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+
+
+try:
+    from .settings_prod import *
+except:
+    pass
