8b9d03eb005d67cccc5bc44c715947b203562ed5
django <django>
2017-05-31 23:55:07 +0300

    Update for server

--- test_project/settings_prod.py
+++ test_project/settings_prod.py
@@ -1,1 +1,14 @@
+DEBUG = False
 ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db1',
+        'USER': 'django_shop',
+        'PASSWORD': 'django_shop12#$',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
