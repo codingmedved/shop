a903d4bfb1d182c3da3d304fb237ae8cb65a67b2
django <codingmedved@gmail.com>
2017-06-01 16:03:24 +0300

    fix for post_save

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from products.models import Product
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from utils.main import disable_for_loaddata
 
 
 class Status(models.Model):
@@ -70,7 +71,7 @@ class ProductInOrder(models.Model):
         super(ProductInOrder, self).save(*args, **kwargs)
 
 
-
+@disable_for_loaddata
 def product_in_order_post_save(sender, instance, created, **kwargs):
     order = instance.order
     all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
